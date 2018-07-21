1d17c86c8a3865352c4e548e812d47e136dc4922
ngt-kn <ngt.kn.dev@gmail.com>
2018-07-21 12:50:50 -0700

    add menu

--- app/src/main/java/com/ngtkn/sketch/MainActivity.java
+++ app/src/main/java/com/ngtkn/sketch/MainActivity.java
@@ -2,6 +2,8 @@ package com.ngtkn.sketch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
