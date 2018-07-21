008d0726ffda3ff59ee2f3e8fb8b3b1d023a5b8d
ngt-kn <ngt.kn.dev@gmail.com>
2018-07-21 03:08:35 -0700

    add touchMoved(), store path of touch event

--- app/src/main/java/com/ngtkn/sketch/view/SketchView.java
+++ app/src/main/java/com/ngtkn/sketch/view/SketchView.java
@@ -9,6 +9,8 @@ import android.graphics.Path;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.HashMap;
@@ -55,7 +57,89 @@ public class SketchView extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-        canvas.drawCircle(getMeasuredWidth()/2, getMeasuredHeight()/2, 78, paintLine);
+        canvas.drawBitmap(bitmap, 0, 0, paintScreen);
+
+        for (Integer key: pathMap.keySet()) {
+            canvas.drawPath(pathMap.get(key), paintLine);
+        }
+
+        canvas.save();
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getActionMasked(); // event type
+        int actionIndex = event.getActionIndex(); // get pointer location up/down
+
+        if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_POINTER_DOWN) {
+            touchStarted(event.getX(actionIndex), event.getY(actionIndex),
+                    event.getPointerId(actionIndex));
+        } else if (action == MotionEvent.ACTION_UP || action == MotionEvent.ACTION_POINTER_UP) {
+            touchEnded(event.getPointerId(actionIndex));
+        } else {
+            touchMoved(event);
+        }
+
+        invalidate(); // redraw the screen
+
+        return true;
+    }
+
+    private void touchMoved(MotionEvent event) {
+
+        for (int i = 0; i < event.getPointerCount(); i++) {
+            int pointerId = event.getPointerId(i);
+            int pointerIndex = event.findPointerIndex(pointerId);
+
+            if (pathMap.containsKey(pointerId)) {
+                float newX = event.getX(pointerIndex);
+                float newY = event.getY(pointerIndex);
+
+                Path path = pathMap.get(pointerId);
+                Point point = previousPointMap.get(pointerId);
+
+                // Calculate distance moved from last update
+                float deltaX = Math.abs(newX - point.x);
+                float deltaY = Math.abs(newY - point.y);
+
+                // Check delta if it is large enough to indicate movement
+                if (deltaX >= TOUCH_TOLERANCE || deltaY >= TOUCH_TOLERANCE){
+                    // move to location
+                    path.quadTo(point.x, point.y,
+                            (newX + point.x)/2,
+                            (newY + point.y)/2);
+
+                    // store the coordinates
+                    point.x = (int) newX;
+                    point.y = (int) newY;
+                }
+            }
+        }
+    }
+
+    private void touchEnded(int pointerId) {
+    }
+
+    private void touchStarted(float x, float y, int pointerId) {
+        Path path;  // holds the path of touch coordinates
+        Point point; // hold last point
+
+        if (pathMap.containsKey(pointerId)) {
+            path = pathMap.get(pointerId);
+            point = previousPointMap.get(pointerId);
+        } else {
+            // this is a new touch event
+            path = new Path();
+            pathMap.put(pointerId, path);
+            point = new Point();
+            previousPointMap.put(pointerId, point);
+        }
+
+        // move to the coordinates of the touch
+        path.moveTo(x, y);
+        point.x = (int) x;
+        point.y = (int) y;
+    }
+    
+    
 }
